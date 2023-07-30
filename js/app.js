208b3df98ad353c035e49302c7b775af14a53704
Sherzod <shortfilmuz@gmail.com>
2023-07-31 00:15:14 +0500

    added second box->show window

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 "use strict";
 
+// Selectors:
+const clickBtn = document.querySelector(".equal_to-btn");
+const openBox = document.querySelector(".add_calc--box");
+
+// Events:
+clickBtn.addEventListener("click", () => {
+    openBox.classList.toggle("show");
+})
