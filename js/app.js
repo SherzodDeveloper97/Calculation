daea8700a8ecb5dbf81d95b85d52b9dee199fb6f
Sherzod <shortfilmuz@gmail.com>
2023-07-31 02:00:16 +0500

    added finished last section

--- js/app.js
+++ js/app.js
@@ -3,16 +3,34 @@
 // Selectors:
 const clickBtn = document.querySelector(".equal_to-btn");
 const openBox = document.querySelector(".add_calc--box");
-const clickOtherBtn = document.getElementById("add_calc--click_btn");
-const openOtherBox = document.querySelector(".calc__section_item");
+const clickSecondBtn = document.getElementById("first--click_btn");
+const openSecondBox = document.querySelector(".calc__section_item");
+const clickThirdBtn = document.querySelector("#second--click_btn");
+const openThirdBox = document.querySelector(".calc__last_section--container");
+
 
 
 // Events:
 clickBtn.addEventListener("click", () => {
     openBox.classList.toggle("show");
-    openOtherBox.classList.remove("active");
+    if(openSecondBox.classList.contains("active")){
+        openSecondBox.classList.remove("active");
+    };
+    if(openThirdBox.classList.contains("active")){
+        openThirdBox.classList.remove("active");
+    }
 });
 
-clickOtherBtn.addEventListener("click", () => {
-    openOtherBox.classList.toggle("active");
+clickSecondBtn.addEventListener("click", () => {
+    if(openThirdBox.classList.contains("active")){
+        openThirdBox.classList.remove("active");
+    }
+    openSecondBox.classList.toggle("active");
 });
+
+clickThirdBtn.addEventListener("click", () => {
+    if(openSecondBox.classList.contains("active")){
+        openSecondBox.classList.remove("active");
+    }
+    openThirdBox.classList.toggle("active");
+})
