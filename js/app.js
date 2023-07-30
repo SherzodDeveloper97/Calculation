39ef846a09074bbcd1f6611b1f8bc9b1181dbe3e
Sherzod <shortfilmuz@gmail.com>
2023-07-31 00:57:31 +0500

    add third box->show window

--- js/app.js
+++ js/app.js
@@ -3,8 +3,16 @@
 // Selectors:
 const clickBtn = document.querySelector(".equal_to-btn");
 const openBox = document.querySelector(".add_calc--box");
+const clickOtherBtn = document.getElementById("add_calc--click_btn");
+const openOtherBox = document.querySelector(".calc__section_item");
+
 
 // Events:
 clickBtn.addEventListener("click", () => {
     openBox.classList.toggle("show");
-})
+    openOtherBox.classList.remove("active");
+});
+
+clickOtherBtn.addEventListener("click", () => {
+    openOtherBox.classList.toggle("active");
+});
